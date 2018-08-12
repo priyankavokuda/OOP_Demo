06082fb2e802e7324c8adb40342983ad265c96a2
Priyanka Subramanya Vokuda <priyanka.subramanya@smail.inf.h-brs.de>
2018-08-12 14:01:40 +0200

    creating test cases

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,25 +1,56 @@
 package de.exxcellent.challenge;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Example JUnit4 test case.
+ * 
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 public class AppTest {
 
-    private String successLabel = "not successful";
+	private String successLabel = "not successful";
+	WeatherData wd = new WeatherData();
+	String weatherDataFileName = "./src/main/resources/de/exxcellent/challenge/weather.csv";
 
-    @Before
-    public void setUp() throws Exception {
-        successLabel = "successful";
-    }
+	@Before
+	public void setUp() throws Exception {
+		successLabel = "successful";
 
-    @Test
-    public void aPointlessTest() {
-        Assert.assertEquals("Expectations met", "successful", successLabel);
-    }
+	}
 
+	@Test
+	public void aPointlessTest() {
+		Assert.assertEquals("Expectations met", "successful", successLabel);
+	}
+
+	/**
+	 * Checks if all the contents are read from csv file
+	 */
+	@Test
+	public void checkAllContentsRead() {
+
+		ArrayList<WeatherData> weatherDataContents = wd
+				.parseContentfromCSV(weatherDataFileName);
+		Assert.assertEquals(weatherDataContents.size(), 30);
+	}
+
+	/**
+	 * Validates the minimum difference calculated by the correct answer
+	 */
+	@Test
+	public void checkminDiff() {
+		ArrayList<WeatherData> weatherDataContents = wd
+				.parseContentfromCSV(weatherDataFileName);
+		ArrayList<Integer> temprSpread = wd
+				.findTemprDifference(weatherDataContents);
+		int temprIndex = wd.findMinOfColumn(temprSpread);
+		Assert.assertEquals((weatherDataContents.get(temprIndex)).getDay(),
+				"14");
+
+	}
 }
